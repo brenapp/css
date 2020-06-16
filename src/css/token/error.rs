c4018bf4b6a010ce145a00753fb221e636c194bb
Brendan McGuire <brendan@bren.app>
2020-06-16 01:45:16 -0400

    Better error display

--- src/css/token/error.rs
+++ src/css/token/error.rs
@@ -6,13 +6,13 @@ use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct ParseError {
-    pub token: tokens::CSSToken,
+    pub token: Option<tokens::CSSToken>,
     pub error_text: &'static str,
     pub at: i32,
 }
 
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.error_text)
+        write!(f, "{} at position {} when parsing attempting to parse {:?}", self.error_text, self.at, self.token)
     }
 }
