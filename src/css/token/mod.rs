10425be2ec7ef5d84cd530306a396c20b2b5c213
Brendan McGuire <brendan@bren.app>
2020-06-16 02:46:34 -0400

    Correctly preprocess string

--- src/css/token/mod.rs
+++ src/css/token/mod.rs
@@ -9,10 +9,28 @@ use tokens::CSSToken;
  * https://www.w3.org/TR/css-syntax-3/#tokenizing-and-parsing
  */
 
+/**
+ * § 3.3 Stream Preprocessing
+ *
+ * \r\n => \n
+ * \r => \n
+ * U+D800 to U+DFFF => U+FFFD REPLACEMENT CHARACTER (�)
+ *
+ * This was taken from the earlier parts of the Servo project (https://github.com/servo/rust-cssparser/blob/0bad1d50ce955bebbc845b5522f5a99d203a04f3/cssparser.rs#L12) and is highly optimizable
+ */
 pub fn preprocess(contents: String) -> String {
     contents
+        .replace("\r\n", "\n")
+        .replace("\r", "\n")
+        .replace("\x00", "\u{FFFD}")
 }
 
+/**
+ * § Tokenization
+ *
+ * 1. Comments
+ * 2. Whitespace
+ */
 pub fn tokenize(contents: String) -> Result<Vec<tokens::CSSToken>, error::ParseError> {
     // Prepare the contents
     let contents = preprocess(contents);
