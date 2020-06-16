66d562cb4a8c53cb8c3a30022715a69e96b6f710
Brendan McGuire <brendan@bren.app>
2020-06-16 01:45:52 -0400

    Use CSSToken::EOF instead of Option<CSSToken> as it's cleaner

--- src/css/token/mod.rs
+++ src/css/token/mod.rs
@@ -2,35 +2,43 @@ pub mod error;
 pub mod parsers;
 pub mod tokens;
 
+use tokens::CSSToken;
+
 /**
  * CSS Tokenizer
  * https://www.w3.org/TR/css-syntax-3/#tokenizing-and-parsing
  */
 
+pub fn preprocess(contents: String) -> String {
+    contents
+}
+
 pub fn tokenize(contents: String) -> Result<Vec<tokens::CSSToken>, error::ParseError> {
+    // Prepare the contents
+    let contents = preprocess(contents);
+
     let mut tokens: Vec<tokens::CSSToken> = Vec::new();
-    let mut iter = contents.chars();
+    let mut iter = contents.chars().peekable();
 
     let mut done = false;
+    let mut position = 0;
 
     while !done {
-        let result = parsers::parse(&mut iter);
+        let result = parsers::parse(&mut iter, &mut position);
 
         // Go through the possible options
         match result {
             // If the parser returned an error propagate that up
             Err(e) => return Err(e),
 
-            // Next, if it returned a token then push it
-            // Else if no token was returned we're at the EOF
+            // If we're good then push the token onto the token list
+            // However if EOF is reached end
             Ok(result) => match result {
-                Some(token) => tokens.push(token),
-                None => done = true,
+                CSSToken::EOF => done = true,
+                _ => tokens.push(result),
             },
         }
     }
-
-    println!("{:?}", tokens);
 
     Ok(tokens)
 }
--- src/css/token/parsers/mod.rs
+++ src/css/token/parsers/mod.rs
@@ -2,22 +2,37 @@ pub mod comment;
 
 use super::error::ParseError;
 use super::tokens::CSSToken;
+use std::cmp::Ordering;
+use std::iter::Peekable;
 use std::str::Chars;
 
 type ParseResult = Result<Option<CSSToken>, ParseError>;
 
 // Looks ahead for some chars returns true if they are there and false otherwise
-pub fn lookahead(iter: &Chars, chars: &[char]) -> bool {
-    // Copy it to look ahead
-    let mut iter = iter.clone().peekable();
+pub fn lookahead(iter: &mut Peekable<Chars>, chars: &[char]) -> bool {
+    // Test the first character before we go and copy the whole iterator
+    let peek = iter.peek();
+    if peek.is_none() {
+        return false;
+    };
 
-    for ch in chars {
-        if let Some(reference) = iter.next() {
-            if reference != *ch {
-                return true;
+    if peek.is_some() && peek.unwrap().partial_cmp(&chars[0]) != Some(Ordering::Equal) {
+        return false;
+    }
+
+    // Now that we've matched the first char we need copy the iterator to compare
+    let mut cmp = iter.clone();
+
+    for char in chars {
+        let next = cmp.next();
+
+        match next {
+            None => return false,
+            Some(ch) => {
+                if ch.partial_cmp(&char) != Some(Ordering::Equal) {
+                    return false;
+                }
             }
-        } else {
-            return false;
         }
     }
 
@@ -27,15 +42,26 @@ pub fn lookahead(iter: &Chars, chars: &[char]) -> bool {
 /**
  * Goes through all the parsers to parse the iterator at that point
  */
-pub fn parse(iter: &mut Chars) -> ParseResult {
+pub fn parse(iter: &mut Peekable<Chars>, position: &mut i32) -> Result<CSSToken, ParseError> {
+    // Check to see if we're at the end
+    if iter.peek().is_none() {
+        return Ok(CSSToken::EOF);
+    }
+
+    // Match Parsers
     // Comment
-    match comment::parse(iter) {
+    match comment::parse(iter, position) {
         Err(e) => return Err(e),
         Ok(result) => match result {
-            Some(token) => return Ok(Some(token)),
+            Some(token) => return Ok(token),
             None => (),
         },
     };
 
-    Ok(None)
+    // If none of the parse errors matched, then we have a parse error
+    Err(ParseError {
+        token: None,
+        error_text: "Unexpected token",
+        at: *position,
+    })
 }
