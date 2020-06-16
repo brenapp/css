fac236ff522d658ddca27d64c58b5d78056a387f
Brendan McGuire <brendan@bren.app>
2020-06-16 01:46:23 -0400

    Correctly consume comments

--- src/css/token/parsers/comment.rs
+++ src/css/token/parsers/comment.rs
@@ -11,54 +11,43 @@ use super::super::error::ParseError;
 use super::super::tokens::CSSToken;
 use super::lookahead;
 use super::ParseResult;
+use std::iter::Peekable;
 use std::str::Chars;
 
 pub const COMMENT_START: [char; 2] = ['/', '*'];
 pub const COMMENT_END: [char; 2] = ['*', '/'];
 
-pub fn parse(points: &mut Chars) -> ParseResult {
+pub fn parse(points: &mut Peekable<Chars>, position: &mut i32) -> ParseResult {
     // Look ahead to see if the next charaters match a character
     let comment_starts = lookahead(points, &COMMENT_START);
 
-    println!("Comment parse {:?}", points);
-
     if comment_starts {
         // Lookahead to the end of the comment
-
         let mut empty = false;
 
         while !empty {
             let end = lookahead(points, &COMMENT_END);
             if end {
+                // Advance the pointer to consume the closing brackets
+                points.next();
+                points.next();
+
+                *position += 2;
+
                 return Ok(Some(CSSToken::Comment));
             }
 
+            *position += 1;
             empty = points.next().is_none();
         }
 
         // If we've reach the end of the iterator return a parse error
         Err(ParseError {
-            token: CSSToken::Comment,
+            token: Some(CSSToken::Comment),
             error_text: "Unexpected End Of File (EOF) when parsing comment body",
-            at: 0,
+            at: *position,
         })
     } else {
         Ok(None)
     }
 }
-
-// for _ in points {
-//     let end = lookahead(points, &COMMENT_END);
-
-//     // If we found the end return
-//     if end {
-//         return Ok(Some(CSSToken::Comment));
-//     }
-// }
-
-// // If we reached the end and there was no end comment, it's a parse error
-// Err(ParseError {
-//     token: CSSToken::Comment,
-//     error_text: "Unexpected EOF when parsing comment",
-//     at: 0,
-// })
