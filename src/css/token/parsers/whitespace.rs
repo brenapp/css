4b43c404b41f05acf7bc0f9df73425c489ee13b6
Brendan McGuire <brendan@bren.app>
2020-06-17 04:12:34 -0400

    Use consume whitespace in parser

--- src/css/token/parsers/whitespace.rs
+++ src/css/token/parsers/whitespace.rs
@@ -1,4 +1,3 @@
-use super::super::check::is_whitespace;
 /**
  * Parses CSS Whitespace
  *
@@ -6,6 +5,8 @@ use super::super::check::is_whitespace;
  *  Ok(Some) => If it matched some whitespace
  *  Ok(None) => No whitespace match
  */
+use super::super::check::is_whitespace;
+use super::super::consume;
 use super::super::tokens::CSSToken;
 use super::ParseResult;
 use std::iter::Peekable;
@@ -17,22 +18,7 @@ pub fn parse(points: &mut Peekable<Chars>, position: &mut i32) -> ParseResult {
 
     match next {
         Some(ch) if is_whitespace(&ch) => {
-            loop {
-                let next = points.peek();
-
-                match next {
-                    None => break,
-                    Some(ch) => {
-                        if !is_whitespace(&ch) {
-                            break;
-                        }
-                    }
-                };
-
-                points.next();
-                *position += 1;
-            }
-
+            consume::whitespace(points, position);
             Ok(Some(CSSToken::Whitespace))
         }
         _ => Ok(None),
