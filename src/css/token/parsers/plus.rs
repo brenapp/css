53801c1134f23674539a67f1a3982e42d19109b8
Brendan McGuire <brendan@bren.app>
2020-06-17 01:29:45 -0400

    Use next_char_equals

--- src/css/token/parsers/plus.rs
+++ src/css/token/parsers/plus.rs
@@ -3,7 +3,7 @@
  * This can be a delimiter (like a + p {})
  * or it can be in a number (like { margin: +5px })
  */
-use super::super::check::{equal, is_number};
+use super::super::check::{is_number, next_char_equals};
 use super::super::consume;
 use super::super::tokens::CSSToken;
 use super::ParseResult;
@@ -11,24 +11,20 @@ use std::iter::Peekable;
 use std::str::Chars;
 
 pub fn parse(points: &mut Peekable<Chars>, position: &mut i32) -> ParseResult {
-    if let Some(ch) = points.peek() {
-        if equal(ch, &'+') {
-            let numeric = is_number(points);
+    if next_char_equals(points, &'+') {
+        let numeric = is_number(points);
 
-            if numeric {
-                match consume::numeric_token(points, position) {
-                    Ok(token) => Ok(Some(token)),
-                    Err(e) => Err(e),
-                }
-            } else {
-                // Consume the plus
-                *position += 1;
-                let plus = points.next().unwrap();
-
-                Ok(Some(CSSToken::Delim(plus)))
+        if numeric {
+            match consume::numeric_token(points, position) {
+                Ok(token) => Ok(Some(token)),
+                Err(e) => Err(e),
             }
         } else {
-            Ok(None)
+            // Consume the plus
+            *position += 1;
+            let plus = points.next().unwrap();
+
+            Ok(Some(CSSToken::Delim(plus)))
         }
     } else {
         Ok(None)
